56dcd56b97896f9929db49a2962754a93589495d
김혜인 <k_hn_n@naver.com>
2020-02-05 07:01:39 +0900

    section01 : 데이터타입 -> 숫자와 산술연산자

---
+++ alone-study/section01/numberTest.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+  <h1>Number & Arithmetic Opertor</h1>
+  <h2>1+1</h2>
+  <?php
+  echo 1+1;
+   ?>
+   <h2>2-1</h2>
+   <?php
+   echo 2-1;
+    ?>
+    <h2>2*2</h2>
+    <?php
+    echo 2*2;
+     ?>
+     <h2>4/2</h2>
+     <?php
+     echo 4/2;
+      ?>
+
+</body>
+</html>
