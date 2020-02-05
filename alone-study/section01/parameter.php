40cd33f9e4ddf06c223b4ca4e8048aea986e6953
김혜인 <k_hn_n@naver.com>
2020-02-05 15:28:41 +0900

    section01 : URL 파라미터

---
+++ alone-study/section01/parameter.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+    <body>
+      안녕하세요. <?php echo $_GET['name'] ;?>님 
+      <?php echo $_GET ['address']; ?>에 사시는군요.
+    </body>
+</html>
