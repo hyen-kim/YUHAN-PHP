7fc172b7b735f4316c2f57c211a015de376bbbf0
김혜인 <k_hn_n@naver.com>
2020-02-05 16:45:06 +0900

    section01 : 파라미터의 활용 수정 02

---
+++ alone-study/section01/info.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php phpinfo();
+    //  echo date('Y-m-d H:i:s');
+      ?>
+  </body>
+</html>
