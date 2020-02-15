b7f1593b8457cea4ea5381781e8634183fb3f1a7
김혜인 <k_hn_n@naver.com>
2020-02-15 14:41:14 +0900

    section04 : 함수의 형식

---
+++ alone-study/section04/basic.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+</head>
+
+<body>
+
+</body>
+<?php
+  function basic(){
+      print("Lorem ipsum dolor1<br>");
+      print("Lorem ipsum dolor2<br>");
+  }
+  basic();
+  basic();
+  basic();
+  ?>
+
+</html>
