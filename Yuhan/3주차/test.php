36d49843a2f3a1107c78f10162d6b3670d3ab2cd
김혜인 <k_hn_n@naver.com>
2020-04-01 16:22:01 +0900

    3주차 과제 : test.php

---
+++ Yuhan/3주차/test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>3주차 2번째 강의</title>
+  </head>
+  <body>
+    <?php
+      /* 자신의 학번과 이름을 출력 */
+      echo "201907033 김혜인";
+     ?>
+  </body>
+</html>
