3a53b84c7494bb0d125d02e4f11c7ce69aa22f36
김혜인 <k_hn_n@naver.com>
2020-04-07 19:23:00 +0900

    4주차 : id,pw 다른 문법

---
+++ Yuhan/4주차/p3-2_a.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>p3-2_a.php</title>
+  </head>
+  <body>
+    <form>
+      아이디 : <?php echo "$id"?> <br>
+      비밀번호 : <input type=text value=<?php echo "$id"?>
+      <br>
+    </form>
+  </body>
+</html>
