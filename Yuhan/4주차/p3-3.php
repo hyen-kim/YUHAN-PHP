0081be3fbead024e848eb07cf20595d479f01c12
김혜인 <k_hn_n@naver.com>
2020-04-07 19:23:31 +0900

    4주차 : table을 php 반복문 삽입

---
+++ Yuhan/4주차/p3-3.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP 4주차 2번째 강의</title>
+  </head>
+  <body>
+    201907033 김혜인
+    <table border="1">
+      <tr>
+        <td>seq.</td>
+        <td>비고</td>
+      </tr>
+    <?php
+    for ($i=1; $i <= 10 ; $i++) {
+      echo "<tr><td>$i</td><td></td></tr>";
+    }
+   ?>
+     </table>
+  </body>
+</html>
