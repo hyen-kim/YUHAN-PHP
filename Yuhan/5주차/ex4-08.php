2e966aa30e259bf59f945919d6fe87799feb5d14
김혜인 <k_hn_n@naver.com>
2020-04-15 16:36:24 +0900

    echo() : '', ""

---
+++ Yuhan/5주차/ex4-08.php
@@ -0,0 +1,13 @@
+<?php
+  echo("Hello PHP<br>");
+  echo "특수문자는 \"이렇게\" 표현합니다.<br>";
+
+  $str = "PHP Web Programming";
+  echo("$str<br>");
+  echo $str;
+  echo "<br>";
+
+  echo 'single 따옴표 안에 있는 변수 : $str <br>';
+  echo "이 문자열은 ", "다중 인자를 ", "사용해서 ", "출력했다.<br>";
+  echo "이 문자열은 "."접합연산을 "."사용해서 "."출력했다.<br>";
+ ?>
