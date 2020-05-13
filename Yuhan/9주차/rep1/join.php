5f1002d0271e64bd8eb245dd6ecef7651385f983
김혜인 <k_hn_n@naver.com>
2020-05-13 14:33:41 +0900

    회원가입 폼 DB연동 2

--- Yuhan/9주차/rep1/join.php
+++ Yuhan/9주차/rep1/join.php
@@ -2,17 +2,33 @@
 <?php
   # php 프로그램과 DB가 연동되는 부분
   $conn = mysql_connect("localhost", "user2020", "php");
-
   if(! $conn) {
     echo "MySQL 서버 접속에 실패했습니다.";
     exit;
   }
-
+  # 우리가 사용하는 DB명
   $db = mysql_select_db("CSE2020");
   if(! $db){
     echo "데이터베이스를 열 수 없습니다.";
     exit;
   }
+  # 전화번호를 합치는부분 pho1, pho2, pho3
+  $pho = $khi_pho1."-".$khi_pho2."-".$khi_pho3;
 
+  # 데이터를 삽입하는 SQL
+  $query = "INSERT INTO b201907033 (id, pw, name, pho, zipcode, sex)
+            VALUES('$khi_id', '$khi_pw', '$khi_name', '$pho', '$khi_zipcode', '$khi_sex')";
+
+  # DB에게 일을 부탁하는 부분
+  $result = mysql_query($query);
+
+  # 에러난 메세지를 출력하는 부분
+  if (! $result) {
+    echo mysql_errno(). " : ". mysql_error(); # no : 번호 / or : 메시지
+    exit; # 프로그램 강제 종료
+  }
+
+  echo $khi_name."님 가입을 환영합니다.";
+  mysql_close($conn);
 
  ?>
--- Yuhan/9주차/rep1/join_form.php
+++ Yuhan/9주차/rep1/join_form.php
@@ -5,40 +5,48 @@
     <title>2차 지필평가</title>
   </head>
   <body>
+    <form method=post action=join.php>
     <table>
     <tr>
       <td>아이디 : </td>
-      <td><input type="text" name="id_hi" size="20"> </td>
+      <td><input type="text" name="khi_id" size="20"> </td>
     </tr>
     <tr>
       <td>비밀번호 : </td>
-      <td><input type="text" name="pw_hi" size="20"></td>
+      <td><input type="text" name="khi_pw" size="20"></td>
+    </tr>
+    <tr>
+      <td>비번확인 : </td>
+      <td><input type="text" name="khi_pwch" size="20"></td>
     </tr>
     <tr>
       <td>이름 : </td>
-      <td><input type="text" name="name_hi" size="20"></td>
+      <td><input type="text" name="khi_name" size="20"></td>
     </tr>
       <tr>
         <td>핸드폰 : </td>
-        <td><select name = "pho1_hi">
-          <option value="">010</option>
-          <?php
-          $j = "01";
-          for ($i=1; $i <= 9 ; $i++) {
-            echo "<option value=$i>$j$i</option>"; }?></select>
-            - <input type="text" name="pho2_hi" size="8">
-            - <input type="text" name="pho3_hi" size="8"></td>
+        <td><select name = "khi_pho1">
+          <option value="010">010</option>
+          <option value="011">011</option>
+          <option value="012">012</option>
+          <option value="013">013</option>
+          <option value="014">014</option>
+          <option value="015">015</option>
+      </select>
+            - <input type="text" name="khi_pho2" size="8">
+            - <input type="text" name="khi_pho3" size="8"></td>
         </tr>
       <tr>
         <td>우편번호 : </td>
-        <td><input type="text" name="zipcode_hi" size="20"></td>
+        <td><input type="text" name="khi_zipcode" size="20"></td>
       </tr>
       <tr>
         <td>성별 : </td>
-        <td><input type="radio" name="sex_hi"> 남자
-            <input type="radio" name="sex_hi"> 여자 </td>
+        <td><input type="radio" name="khi_sex" value="M"> 남자
+            <input type="radio" name="khi_sex" value="F"> 여자 </td>
       </tr>
     </table>
     <input type="submit" value="회원가입">
+  </form>
   </body>
 </html>
