713f1ea456d078ca7452c5ce334cf1b57ac7c2f4
김혜인 <k_hn_n@naver.com>
2020-05-17 23:22:32 +0900

    수정 2

---
+++ Yuhan/10주차/rep2/login_form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>rep2 login</title>
+  </head>
+  <body>
+    <form method=post action=login.php>
+      아이디 : <input type="text" name="login_id"> <br>
+      비밀번호 : <input type="text" name="login_pw"> <br>
+    <input type="submit" value="login">
+  </form>
+  </body>
+</html>
